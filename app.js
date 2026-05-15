ac6b7bb40b19d9dbe152dc68ece3b416c997cbb5
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2026-05-14 20:53:48 -0400

    feat: added home page.
    Home page will display basic site information to users.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const { indexRouter } = require("./routes/index");
+const path = require("node:path");
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
 
 app.listen(process.env.PORT);
