81ad3036e86b6b9d849467bb372a71d930287ef5
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2026-05-23 21:58:51 -0400

    feat: parts now add to the database

--- models/update.js
+++ models/update.js
@@ -1,4 +1,5 @@
 const ForbiddenError = require("../error/forbidden");
+const InternalServiceError = require("../error/internalServiceError");
 const { pool } = require("./pool");
 
 async function addCategory(name, password) {
@@ -9,4 +10,24 @@ async function addCategory(name, password) {
   }
 }
 
-module.exports = { addCategory };
+async function addPart(name, price, category, manufacturer, password) {
+  if (password == process.env.PASSWORD) {
+    try {
+      await pool.query(
+        `INSERT INTO parts (name, price, category_id, manufacturer_id)
+        VALUES ($1, $2,
+        (SELECT id FROM categories WHERE name = $3),
+        (SELECT id FROM manufacturers WHERE name = $4));`,
+        [name, price, category, manufacturer],
+      );
+    } catch {
+      throw new InternalServiceError(
+        "500: Internal Service Error. Database Error",
+      );
+    }
+  } else {
+    throw new ForbiddenError("403: Forbidden. Incorrect Password");
+  }
+}
+
+module.exports = { addCategory, addPart };
--- routes/part.js
+++ routes/part.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const { getCategories, getManufacturers } = require("../models/query");
 const InternalServiceError = require("../error/internalServiceError");
+const { addPart } = require("../models/update");
 
 const partRouter = Router();
 
@@ -12,6 +13,20 @@ partRouter.get("/add", async (req, res, next) => {
     next(new InternalServiceError("Database error"));
   }
   res.render("add-part", { title: "Add-Part", manufacturers, categories });
+});
+partRouter.post("/add", async (req, res, next) => {
+  try {
+    await addPart(
+      req.body.name,
+      req.body.price,
+      req.body.category,
+      req.body.manufacturer,
+      req.body.password,
+    );
+    res.redirect("/");
+  } catch {
+    next(new InternalServiceError("Database error"));
+  }
 });
 
 module.exports = { partRouter };
